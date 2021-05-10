8e67b2fa7f6a7f0126fad25828f290aa14b0af4c
ICE-sherbet <ice.chiruno9@gmail.com>
2021-05-10 13:55:25 +0900

    プロジェクト ファイルを追加します。

---
+++ GradeJudg/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GradeJudg
+{
+    public partial class Form1 : Form
+    {
+        private int[] rankClass = new int[] {60, 70, 80,90,100};
+        private string[] rankClassString = new string[] {"D:不可","C:可","B:良","A:優","S:秀"};
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonJudg_Click(object sender, EventArgs e)
+        {
+            label2.Text = rankclassify(textBoxScore.getNum());
+        }
+        private string rankclassify(int score)
+        {
+            for (int i = 0; i < rankClass.Length; i++)
+            {
+                if (score < rankClass[i]) return rankClassString[i];
+            }
+            return rankClassString[rankClassString.Length -1];
+        }
+    }
+}
